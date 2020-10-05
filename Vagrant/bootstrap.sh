e4c6981adf1264654be5dafe21a2e337426ab1e2
Jonathan Echavarria <jechavarria@reliaquest.com>
2020-10-05 16:36:25 -0400

    start of AWS and some minor tweaks

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -76,18 +76,18 @@ fix_eth1_static_ip() {
   }' >>/etc/dhcp/dhclient.conf
   netplan apply
   # Fix eth1 if the IP isn't set correctly
-  ETH1_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+  ETH1_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep 38)
   if [ "$ETH1_IP" != "192.168.38.105" ]; then
     echo "Incorrect IP Address settings detected. Attempting to fix."
     ifdown eth1
     ip addr flush dev eth1
     ifup eth1
-    ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
+    ETH1_IP=$(ifconfig eth1 | grep 'inet' | cut -d ':' -f 2 | cut -d ' ' -f 10)
     if [ "$ETH1_IP" == "192.168.38.105" ]; then
       echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.105"
     else
       echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth1. Exiting because this will cause problems with other VMs."
-      exit 1
+      # exit 1
     fi
   fi
 
