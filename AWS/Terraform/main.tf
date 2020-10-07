76a52145f79ffe1ca646be1152d1d880912b9439
Jonathan Echavarria <jechavarria@reliaquest.com>
2020-10-07 12:13:56 -0400

    installing ELK via AWS tf

--- AWS/Terraform/main.tf
+++ AWS/Terraform/main.tf
@@ -189,7 +189,7 @@ resource "aws_instance" "logger" {
       "sudo adduser --disabled-password --gecos \"\" vagrant && echo 'vagrant:vagrant' | sudo chpasswd",
       "sudo mkdir /home/vagrant/.ssh && sudo cp /home/ubuntu/.ssh/authorized_keys /home/vagrant/.ssh/authorized_keys && sudo chown -R vagrant:vagrant /home/vagrant/.ssh",
       "echo 'vagrant    ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers",
-      "sudo git clone https://github.com/clong/DetectionLab.git /opt/DetectionLab",
+      "sudo git clone https://github.com/Und3rf10w/DetectionLabELK.git /opt/DetectionLab",
       "sudo sed -i 's/eth1/ens5/g' /opt/DetectionLab/Vagrant/bootstrap.sh",
       "sudo sed -i 's/ETH1/ens5/g' /opt/DetectionLab/Vagrant/bootstrap.sh",
       "sudo sed -i 's/eth1/ens5/g' /opt/DetectionLab/Vagrant/resources/suricata/suricata.yaml",
@@ -199,6 +199,8 @@ resource "aws_instance" "logger" {
       "sudo chmod +x /opt/DetectionLab/Vagrant/bootstrap.sh",
       "sudo apt-get -qq update",
       "sudo /opt/DetectionLab/Vagrant/bootstrap.sh",
+      "sudo chmod +x /opt/DetectionLab/Vagrant/ELK.sh",
+      "sudo /opt/DetectionLab/Vagrant/ELK.sh"
     ]
 
     connection {
